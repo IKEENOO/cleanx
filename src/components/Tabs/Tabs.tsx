f030147dc43056979bb7e635268cae4afd2a213e
IKEENOO <kudryavtsev.dev@gmail.com>
2024-05-05 21:08:46 +0300

    feat: create swiper component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Image from "next/image";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import "./style.css";
+
+
+
+const Tabs = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      loop={true}
+    >
+      
+      <SwiperSlide className='SwiperSlide'>
+        
+        <p className="py-2">
+          Pranie chemiczne materaca
+        </p>
+
+        <Image
+          className='swiper__image'
+          src="/assets/materaca.jpg"
+          alt="materaca"
+          width={300}
+          height={300}
+          priority
+        />
+
+      </SwiperSlide>
+      
+      <SwiperSlide className='SwiperSlide'>
+        
+        <p className="py-2">
+          Pranie chemiczne mebli
+        </p>
+
+        <Image
+          className='swiper__image'
+          src="/assets/mebli.jpg"
+          alt="mebli"
+          width={300}
+          height={300}
+          priority
+        />
+
+      </SwiperSlide>
+      
+      <SwiperSlide className='SwiperSlide'>
+        
+        <p className="py-2">
+          Pranie chemiczne dywanu
+        </p>
+
+        <Image
+          className='swiper__image'
+          src="/assets/duwanu.jpg"
+          alt="duwanu"
+          width={300}
+          height={300}
+          priority
+        />
+
+      </SwiperSlide>
+      
+      <SwiperSlide className='SwiperSlide'>
+        
+        <p className="py-2">
+          Pranie wykładzin biurowych
+        </p>
+
+        <Image
+          className='swiper__image'
+          src="/assets/biurowych.jpg"
+          alt="biurowych"
+          width={300}
+          height={300}
+          priority
+        />
+
+      </SwiperSlide>
+
+    </Swiper>
+  )
+}
+
+export default Tabs;
