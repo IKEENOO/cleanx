42326320a2f55d1348752dd3439f7dd76c187a5d
IKEENOO <kudryavtsev.dev@gmail.com>
2024-05-05 18:59:13 +0300

    feat: create upholsterycleaning component

---
+++ src/components/UpholsteryCleaning/UpholsteryCleaning.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./style.css";
+
+const UpholsteryCleaning = () => {
+  return (
+    <div className="box">
+            <h3>Czym jest ekstrakcyjne pranie tapicerki?</h3>
+            <p>
+                Proces prania tapicerki meblowej metodą ekstrakcyjną składa się z kilku etapów:
+            </p>
+            <ol>
+                <li>
+                    <b>Zidentyfikowanie plam</b> 
+                    przed rozpoczęciem prania pracownik naszej firmy ocenia stan tapicerki, stopień zabrudzenia oraz identyfikuje materiał. Na tym etapie pracownik planuje, jakie detergenty będzie zastosowywać podczas pranie tapicerki.
+                </li>
+                <li>
+                    <b>Przygotowanie powierzchni tapicerki</b>
+                    — przed rozpoczęciem czyszczenia w razie potrzeby usuwamy z tapicerki wszelkie luźne zanieczyszczenia, takie jak kurz czy sierść. Jeśli na tapicerce znajdują się plamy, pracownik usuwa przed praniem używająć specjalnych odpamiaczy do usuwania plam.
+                </li>
+                <li>
+                    <b>Pre-spray</b>
+                    — Następnie na tapicerkę lub dywan nakładany jest specjalny preparat, który działa jak środek zmiękczający. Preparat ten ułatwia rozpuszczenie zanieczyszczeń, w tym olejów i tłuszczów, co ułatwia ich usunięcie.
+                </li>
+                <li>
+                    <b>Ekstrakcja</b>
+                    — po upływie ~10-15 minut, tapicerka zostaje prana za pomocą specjalnego odkurzacza piorącego, które wtryskuje gorącą wodę pod wysokim ciśnieniem wraz z detergentem i jednocześnie wciąga ją z powrotem, usuwając zanieczyszczenia z tapicerki.
+                </li>
+                <li>
+                    <b>Płukanie</b>
+                    — Płukanie tapicerki to proces, który przeprowadza się po wykonaniu prania tapicerki meblowej. Polega na dokładnym spłukaniu detergentu i zanieczyszczeń z powierzchni tapicerki za pomocą specjalnej płukanki.
+                </li>
+                <li>
+                    <b>Suszenie</b>
+                    — po praniu ekstrakcyjnym, tapicerka meblowa suszy się za pomocą specjalistycznego sprzętu. Nasza firma stosuje specjalne wentylatory i suszarki do szybkiego i skutecznego suszenia, dzięki czemu proces schnięcia jest szybszy i bardziej skuteczny.
+                </li>
+            </ol>
+            <p>
+                Podsumowując, pranie tapicerki meblowej metodą ekstrakcyjną jest bardzo skuteczne w usuwaniu zanieczyszczeń i plam z powierzchni tapicerowanych. Nasza firma gwarantuje usunięcie plam i zapachów do 100%.
+            </p>
+        </div>
+  )
+}
+
+export default UpholsteryCleaning
