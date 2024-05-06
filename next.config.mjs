310a38a33f98c7100a58ec2b8ace683d8ef473e4
IKEENOO <kudryavtsev.dev@gmail.com>
2024-05-06 22:27:29 +0300

    feat: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export'
+};
+module.exports = {
+    images: {
+      domains: ['localhost']
+    }
+  }
 
 export default nextConfig;
